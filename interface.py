c992fe110391c3399b174e66eb397d1173e50bd2
james08m <james08major@gmail.com>
2015-01-28 14:40:16 -0500

    Create class Win_Main
    Create class Win_Main and use it in run.py instead of directly
    displaying the window in a while loop

--- interface.py
+++ interface.py
@@ -1,5 +1,55 @@
 import pygame
+import datetime
 
+# Class of the Main window
+class Win_Main:
+    def __init__(self, screen):
+        self.screen = screen
+        self.open = True
+        # Images  -----------------------------------------------
+        self.IMG_Contacts = pygame.image.load("icons/contacts.png").convert()
+        self.IMG_Settings = pygame.image.load("icons/settings.png").convert()
+        self.IMG_Mail = pygame.image.load("icons/mail.png").convert()
+
+        # Buttons -----------------------------------------------
+        self.BTN_Contacts = Button((3,20,60,60))
+        self.BTN_Contacts.setIcon(self.IMG_Contacts)
+
+        self.BTN_Settings = Button((66,20,60,60))
+        self.BTN_Settings.setIcon(self.IMG_Settings)
+
+        self.BTN_Mail = Button((129,20,60,60))
+        self.BTN_Mail.setIcon(self.IMG_Mail)
+
+    def exect(self):
+        while self.open:
+            # Event handling
+            event = pygame.event.poll()
+            if event.type == pygame.MOUSEBUTTONDOWN: # If mouse down
+                pos = pygame.mouse.get_pos()
+            elif event.type == pygame.KEYDOWN: # If key down (not supose to happen)
+                if event.key == pygame.K_ESCAPE:
+                    self.open = False
+
+            # Display components
+            self.screen.fill((0,0,0))
+
+            # Display header
+            current_time = datetime.datetime.now().time() # Get time
+            font = pygame.font.SysFont("verdana", 10)
+            time_str = str(current_time.hour) + ":" + str(current_time.minute)
+            time = font.render(time_str, 1, (255,255,255))
+            self.screen.blit(time, (200, 3))
+
+            # Display buttons
+            self.BTN_Contacts.draw(self.screen)
+            self.BTN_Settings.draw(self.screen)
+            self.BTN_Mail.draw(self.screen)
+            pygame.display.flip()
+
+
+
+# Button class
 class Button:
     def __init__(self, rect):
         self.rect = rect
--- run.py
+++ run.py
@@ -4,7 +4,6 @@ import os
 from user import *
 from log import *
 from interface import *
-import datetime
 
 # Initialization ----------------------------------------
 print("Initialization...")
@@ -29,48 +28,10 @@ log.add("Set display...")
 pygame.init()
 window  = pygame.display.set_mode((240,320))
 
-# Buttons -----------------------------------------------
-img_btn1 = pygame.image.load("icons/contacts.png").convert()
-button1 = Button((3,20,60,60))
-button1.setIcon(img_btn1)
-
-img_btn2 = pygame.image.load("icons/settings.png").convert()
-button2 = Button((66,20,60,60))
-button2.setIcon(img_btn2)
-
-img_btn3 = pygame.image.load("icons/mail.png").convert()
-button3 = Button((129,20,60,60))
-button3.setIcon(img_btn3)
-
-# Images ------------------------------------------------
-img_bg = pygame.image.load("images/bg.jpg").convert()
-
 # Mainloop ----------------------------------------------
 log.add("Mainloop...")
-Running = True
-while Running:
-	event = pygame.event.poll()
-	if event.type == pygame.MOUSEBUTTONDOWN:
-		pos = pygame.mouse.get_pos() # Get mouse position if touch
-	elif event.type == pygame.KEYDOWN:
-		if event.key == pygame.K_ESCAPE:
-			Running = False
-	
-	# Set black background
-	window.fill((0,0,0))
-	
-	# Display header
-	current_time = datetime.datetime.now().time() # Get time
-	font = pygame.font.SysFont("verdana", 10)
-	time_str = str(current_time.hour) + ":" + str(current_time.minute)
-	time = font.render(time_str, 1, (255,255,255))
-	window.blit(time, (200, 3))
-	
-	
-	# Display buttons
-	button1.draw(window)
-	button2.draw(window)
-	button3.draw(window)
-	pygame.display.flip()
+WIN_Main = Win_Main(window)
+WIN_Main.exect()
+
 
 log.add("Quit")
