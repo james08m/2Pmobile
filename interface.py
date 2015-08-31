92e2a848764c2a712ed5e3931af5003822610b04
james08m <james08major@gmail.com>
2015-08-30 23:55:47 -0400

    Correct indentations errors [interface.py]

--- interface.py
+++ interface.py
@@ -66,10 +66,10 @@ class Win_Main:
             # Display header
             font = pygame.font.SysFont("verdana", 13)
             H_Time = font.render(getTimeToMinutes(), 1, (255,255,255))
-			H_User = font.render(getUser(), 1, (255,255,255))
+            H_User = font.render(getUser(), 1, (255,255,255))
 
             self.screen.blit(H_Time, (275, 8))
-			self.screen.blit(H_User, (10, 8))
+            self.screen.blit(H_User, (10, 8))
 
             # Display buttons
             self.BTN_Phone.draw(self.screen)
