20db8248967ed334e8f6859cbd2a21bfea90696b
james08m <james08major@gmail.com>
2015-09-01 10:39:56 -0400

    Correct indentations errors [user.py]

--- user.py
+++ user.py
@@ -3,19 +3,18 @@ import getpass
 
 # User class
 class User:
-        # Constructor
-        def __init__(self):				
-		
-		# Return actual linux user connected
-		def getUsername(self):
-			return getpass.getuser()
+	# Constructor
+	def __init__(self):				
 
-		# Return user filepath
-		def getFilepath(self):
-			return os.environ['HOME']
+	# Return actual linux user connected
+	def getUsername(self):
+	return getpass.getuser()
+
+	# Return user filepath
+	def getFilepath(self):
+	return os.environ['HOME']
 
 
 # If run as main ...
 if __name__ == '__main__':
-    # Test zone
 	
