963fd33da1551ee3969523b7ec879b15aa6d759c
james08m <james08major@gmail.com>
2015-09-01 10:52:24 -0400

    Minor change [run.py]

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 # Import
 import pygame
 import os
-#from user import *
 #from log import *
 from interface import *
 from utilities import *
@@ -23,11 +22,11 @@ os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
 #log.add("Init screen display...")
 pygame.init()
 pygame.mouse.set_visible(False)
-screen  = pygame.display.set_mode((320,480))
+display  = pygame.display.set_mode((320,480))
 
 # Mainloop ----------------------------------------------
 #log.add("Lunching...")
-window = WIN_Main(screen, 0)
+window = WIN_Main(display, 0)
 window.exect()
 
 #log.add("Saving user..")
