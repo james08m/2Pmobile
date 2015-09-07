100d240187474f2ee2989d4aad48276e82729807
james08m <james08major@gmail.com>
2015-09-07 11:39:40 -0400

    Correct path name for background [interface.py]

--- interface.py
+++ interface.py
@@ -12,7 +12,7 @@ class WIN_Main:
 
         # Images -----------------------------------------------
         #self.IMG_Background = pygame.image.load(User.getFilepath()+"/Pictures/bg.jpg")
-        self.IMG_Background = pygame.image.load("/home/j08m/Pictures/bg.jpg")
+        self.IMG_Background = pygame.image.load("/home/pi/Pictures/bg.jpg")
         self.IMG_Phone = pygame.image.load("icons/iphone.png")
         self.IMG_Messages = pygame.image.load("icons/imessages2.png")
         self.IMG_Music = pygame.image.load("icons/imusic.png")
